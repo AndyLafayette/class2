9287116a6d3e6d6586e2e8c4c4aa662d6edef65d
psyrj2 <psyrj2@nottingham.ac.uk>
2019-06-11 03:41:36 -0400

    second

--- WordCounterMapper.java
+++ WordCounterMapper.java
@@ -10,12 +10,15 @@ public class WordCounterMapper
 public void map(LongWritable key, Text value, Context context)
 throws IOException, InterruptedException {
 String[] target = {"hackathon", "Dec", "Chicago", "Java"};
+for (int i = 0; i < target.length; i++){
+	context.write(new Text(target[i]), new IntWritable(0));
+}
 String line = value.toString();
 String[] strs = line.split("[^a-zA-Z]");
 for (String t : target){
 	for (String str: strs){
 		if (str.toLowerCase().equals(t.toLowerCase())) {
-		    context.write(new Text(str), new IntWritable(1));
+		    context.write(new Text(t), new IntWritable(1));
 			break;
 		}
 	}
